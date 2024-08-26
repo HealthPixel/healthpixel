75dc1ee3b0d1e2d3e2163e6b6297e0e19f50da52
Tai Keith <keithsteve.ks@hotmail.com>
2024-08-26 17:25:09 +0300

    [Feature]: Worked on the Models required for the project and registered them with the DB storage engine

---
+++ models/doctor.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines the class Doctor"""
+import models
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+
+class Doctor(BaseModel, Base):
+    """
+    Simple model of a Doctor object
+
+    Attributes:
+        first_name (str): First name of the Doctor
+        last_name (str): Last name of the Doctor
+        phone_number (str): Phone number of the Doctor
+        email (str): Email address of the Doctor
+        specialization (str): Doctor's specialization
+    """
+    __tablename__ = 'doctors'
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    phone_number = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False)
+    specialization = Column(String(50), nullable=False)
---
+++ models/patient.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Defines the class Patient"""
+import models
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Date
+from sqlalchemy.orm import relationship
+
+
+class Patient(BaseModel, Base):
+    """
+    Simple model of a Patient object
+
+    Attributes:
+        first_name (str): First name of the Patient
+        last_name (str): Last name of the Patient
+        date_of_birth (date): Date of birth of the Patient
+        gender (str): Gender of the Patient
+        blood_group (str): Blood group of the Patient
+        address (str): Address of the Patient
+        zipcode (str): Zipcode of the Patient's address
+        phone_number (str): Phone number of the Patient
+        email (str): Email of the Patient
+        emergency_contact (str): Emergency contact details of the Patient
+                                 (name & phone number)
+    """
+    __tablename__ = 'patients'
+
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    date_of_birth = Column(Date, nullable=False)
+    gender = Column(String(30), nullable=False)
+    blood_group = Column(String(20), nullable=False)
+    address = Column(String(255), nullable=True)
+    zipcode = Column(String(20), nullable=True)
+    phone_number = Column(String(20), nullable=False)
+    email = Column(String(50), nullable=False)
+    emergency_contact_name = Column(String(50), nullable=False)
+    emergency_contact_phone = Column(String(50), nullable=False)
