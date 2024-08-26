6a72ad18ad87b0fe462f6bac0adc40d97711062c
Fortune Iheanacho <nachodev369@gmail.com>
2024-08-27 00:55:48 +0100

    Update on Auth Integration

--- api/app.py
+++ api/app.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+User Profile route for the Flask Application
+"""
+from flask import Flask, request, render_template, redirect, url_for, flash
+from api.auth import auth
+
+app = Flask(__name__)
+
+app.register_blueprint(auth)
+
+app.route('/healthpixel/')
+def healthpixel():
+    pass
+
+
+if __name__ == "__main__":
+    app.run()
--- api/auth.py
+++ api/auth.py
@@ -2,18 +2,18 @@
 """
 Creates a new User and Integrates with Backend Database
 """
-from models import BaseModel, Doctor, Patient, storage
+from models import BaseModel, Doctor, storage
 from models.base_model import Base
+from flask_sqlalchemy import SQLAlchemy
 from flask import Blueprint, request, render_template, redirect, url_for, flash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
-
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('healthpixels/register', methods=['GET'])
+@auth.route('healthpixels/register', methods=['GET', 'POST'])
 def register():
     hashed_password = ''
     if request.method == "GET":
@@ -25,28 +25,41 @@ def register():
         password = request.form['password']
         conf_password = request.form['conf_password']
 
-        if (password == conf_password):
-            hashed_password = generate_password_hash(password, method='sha256')
+        if password != conf_password:
+            flash('Passwords do not match')
+            return redirect(url_for('auth.register'))
 
-        new_user = Doctor(first_name=first_name, last_name=last_name,
-                          phone_number=tel, email=email,
+        hashed_password = generate_password_hash(password, method='sha256')
+
+        new_user = Doctor(first_name=first_name,
+                          last_name=last_name,
+                          phone_number=tel,
+                          email=email,
                           specialization=specialization,
-                          hashed_password=password)
+                          password=hashed_password)
 
         try:
             storage.new(new_user)
             storage.save(new_user)
             flash("User Created Successfully!!!")
+            return redirect(url_for('auth.login'))
         except:
             flash('Username already exists')
-            return redirect(url_for('register'))
-    return (render_template('signup.html'))
+            return redirect(url_for('auth.register'))
+    return (render_template('register.html'))
 
 
-@auth.route('healthpixels/login', method=['GET'])
-def login():
-    email = request.form['email']
-    id = request.form['id']
-    password = request.form['password']
+# @auth.route('healthpixels/login', method=['GET'])
+# def login():
+#     if request.method == 'GET':
+#         email = request.form['email']
+#         id = request.form['id']
+#         password = request.form['password']
 
-    doctor = Doctor.
+#         doctor = Doctor.query.filter_by(email=email).first()
+#         if doctor and check_password_hash(doctor.password, password) and doctor.id == id:
+#             login_user(doctor)
+#             flash("Login Successfull")
+#         else:
+#                 flash('Login Unsuccessful. Please check username and password.')
+#     return render_template('login.html')
