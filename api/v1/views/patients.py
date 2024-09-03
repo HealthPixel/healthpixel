930f52f041b55eeb9fa163993d6e5aa414515a3c
Fortune Iheanacho <nachodev369@gmail.com>
2024-09-03 13:27:00 +0100

    Update on Patient and Medicatio API Endpoints

--- api/v1/views/patients.py
+++ api/v1/views/patients.py
@@ -4,6 +4,7 @@ This module creates view for Patient objects
 """
 
 from flask import jsonify, request, abort
+from sqlalchemy import text
 from api.v1.views import app_views
 from models import storage
 from models.doctor import Doctor
@@ -11,14 +12,14 @@ from models.patient import Patient
 from models.access_log import Access_Log
 
 
-@app_views.route('/doctors/patients', methods=['GET'], strict_slashes=False)
+@app_views.route('/doctor/patients', methods=['GET'], strict_slashes=False)
 def get_patients():
     """Retrieves a list of all Patients"""
     patients = storage.all(Patient).values()
     return jsonify([patient.to_dict() for patient in patients]), 201
 
 
-@app_views.route('/doctors/<doctor_id>/patients/<patient_id>', methods=['GET'],
+@app_views.route('/doctor/<doctor_id>/patient/<patient_id>', methods=['GET'],
                  strict_slashes=False)
 def get_a_patient(doctor_id, patient_id):
     """Retrieves a Patient object based on its ID"""
@@ -31,8 +32,77 @@ def get_a_patient(doctor_id, patient_id):
         abort(400, "Patient not found")
 
     # Log the access
-    access_log = Access_Log(user_id=doctor_id, patient_id=patient_id)
+    action_taken = "Retrieve a Patient"
+    access_log = Access_Log(user_id=doctor_id, patient_id=patient_id, action_taken=action_taken)
     storage.new(access_log)
     storage.save()
 
     return jsonify(patient.to_dict()), 201
+
+
+@app_views.route('/doctor/<doctor_id>/patient/<patient_id>', methods=['PUT'], strict_slashes=False)
+def update_a_patient(doctor_id, patient_id):
+    """Updates a Patient object based on its ID"""
+    doctor = storage.get(Doctor, doctor_id)
+    patient = storage.get(Patient, patient_id)
+    if not doctor:
+        abort(400, "Doctor not found")
+    if not patient:
+        abort(404, "Patient not found")
+
+    data = request.get_json()
+    if not data:
+        abort(400, "Not a JSON")
+
+    ignored_keys = ['id', 'created_at', 'updated_at'] # These keys can't be updated
+    for key, value in data.items():
+            if key not in ignored_keys:
+                if hasattr(patient, key):
+                    setattr(patient, key, value)
+
+    try:
+        # Log the access
+        action_taken = (
+            f"{patient.first_name} {patient.last_name}'s record was UPDATED by "
+            f"Doctor {doctor.first_name} {doctor.last_name}"
+            )
+        access_log = Access_Log(user_id=doctor_id, patient_id=patient_id, action_taken=action_taken)
+        storage.new(access_log)
+
+        # Ssave all chnages to database
+        storage.save()
+    except Exception as e:
+        abort(500, f"An error occured while saving the Patient: {str(e)}")
+
+    return jsonify(patient.to_dict()), 201
+
+
+@app_views.route('/doctor/<doctor_id>/patient/<patient_id>', methods=['DELETE'], strict_slashes=False)
+def delete_a_patient(doctor_id, patient_id):
+    """Deletes a Patient object based on its ID"""
+    doctor = storage.get(Doctor, doctor_id)
+    patient = storage.get(Patient, patient_id)
+
+    if not doctor:
+        abort(400, "Doctor not found")
+    if not patient:
+        abort(404, "Patient not found")
+
+    # Log the access
+    action_taken = (
+        f"{patient.first_name} {patient.last_name}'s record was DELETED by "
+        f"Doctor {doctor.first_name} {doctor.last_name}"
+        )
+    access_log = Access_Log(user_id=doctor_id, patient_id=patient_id, action_taken=action_taken)
+    storage.new(access_log)
+
+    # Disable foreign key checks before deleting
+    storage._DBStorage__session.execute(text('SET FOREIGN_KEY_CHECKS = 0'))
+
+    # Deletes the Patient
+    storage.delete(patient)
+    storage.save()
+
+    # Enables foreign key checks after deleting
+    storage._DBStorage__session.execute(text('SET FOREIGN_KEY_CHECKS = 1'))
+    return jsonify({}), 200
