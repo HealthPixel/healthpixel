3dae92acecbaffb91ada9d3c258d05636af0a735
Fortune Iheanacho <nachodev369@gmail.com>
2024-09-06 00:19:29 +0100

    Update on Error page and Register Patient page

--- api/v1/app.py
+++ api/v1/app.py
@@ -38,7 +38,11 @@ def healthpixel():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('error.html'), 404
+    return render_template('error404.html'), 404
+
+@app.errorhandler(403)
+def page_not_found(e):
+    return render_template('error403.html'), 403
 
 if __name__ == "__main__":
     """ Main Function """
