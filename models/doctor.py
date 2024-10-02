6a6efaeb937d5dd3c55ddbd3486abfef1820f72b
Tai Keith <keithsteve.ks@hotmail.com>
2024-10-02 21:52:27 +0300

    [Feature]: Updated Doctor and Patient models for itsdangerous tokenization

--- models/doctor.py
+++ models/doctor.py
@@ -6,6 +6,8 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from flask_login import UserMixin
+from itsdangerous import URLSafeTimedSerializer as Serializer
+from flask import current_app
 
 
 class Doctor(BaseModel, Base, UserMixin):
@@ -26,3 +28,19 @@ class Doctor(BaseModel, Base, UserMixin):
     email = Column(String(50), nullable=False, unique=True)
     specialization = Column(String(50), nullable=False)
     password = Column(String(128), nullable=False)
+
+
+    def get_reset_token(self):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        return s.dumps({'user_id': self.id})
+
+    @staticmethod
+    def verify_reset_token(token):
+        from models import storage
+
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except Exception:
+            return None
+        return storage.query(Doctor).get(user_id)
